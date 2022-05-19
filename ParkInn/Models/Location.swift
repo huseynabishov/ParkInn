374435e68de02165950f6b0a6c31a39790e2cdf1
huseynabishov <92877169+huseynabishov@users.noreply.github.com>
2022-05-19 20:11:55 +0300

    Assets and Custom Data Model

---
+++ ParkInn/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  ParkInn
+//
+//  Created by Huseyn Abishov on 19.05.2022.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
