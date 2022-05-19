27e13a0972b56a38158c85b6fb81daf587937213
huseynabishov <92877169+huseynabishov@users.noreply.github.com>
2022-05-19 20:04:17 +0300

    Initial Commit

---
+++ ParkInn/ParkInnApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkInnApp.swift
+//  ParkInn
+//
+//  Created by Huseyn Abishov on 19.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ParkInnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
