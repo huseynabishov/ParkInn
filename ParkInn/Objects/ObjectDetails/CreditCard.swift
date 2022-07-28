f7d9b3d27325d5b78aace013349424b384ca0bb0
huseynabishov <92877169+huseynabishov@users.noreply.github.com>
2022-07-28 16:21:42 +0400

    Implemented CreditCard view

--- ParkInn/Objects/ObjectDetails/CreditCard.swift
+++ ParkInn/Objects/ObjectDetails/CreditCard.swift
@@ -18,10 +18,63 @@ struct CreditCard<Content>: View where Content: View {
 
 struct CreditCardFront: View {
     var body: some View {
-        VStack {
-            Text("FRONT").foregroundColor(Color.white)
+        VStack(alignment: .leading) {
+            
+            HStack(alignment: .top) {
+                Image(systemName: "checkmark.circle.fill")
+                    .foregroundColor(Color.white)
+                
+                Spacer()
+                
+                Text("VISA")
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 24))
+                    .fontWeight(.bold)
+                
+            }
+            
+            Spacer()
+            
+            Text("**** **** **** 2864")
+                .foregroundColor(Color.white)
+                .font(.system(size: 32))
+            
+            Spacer()
+            
+            HStack {
+                
+                VStack(alignment: .leading) {
+                    Text("CARD HOLDER")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.gray)
+                    
+                    Text("JACK WILLIAMS")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                }
+                
+                Spacer()
+                
+                VStack {
+                    Text("EXPIRES")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.gray)
+                    Text("02/23")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                }
+                
+            }
+            
+            
+            
         }.frame(width: 300, height: 200)
-            .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue] ), startPoint: .leading, endPoint: .trailing))
+            .padding()
+            .background(LinearGradient(gradient: Gradient(colors: [Color("AccentColorP"), Color("AccentColor")] ), startPoint: .topLeading, endPoint: .bottomTrailing))
             .cornerRadius(10)
     }
 }
@@ -29,7 +82,28 @@ struct CreditCardFront: View {
 struct CreditCardBack: View {
     var body: some View {
         VStack {
-            Text("").foregroundColor(Color.white)
+            
+            Rectangle()
+                .frame(maxWidth: .infinity, maxHeight: 20)
+                .padding([.top])
+            
+            Spacer()
+            
+            HStack {
+                
+                Text("123")
+                    .foregroundColor(Color.black)
+                    .rotation3DEffect(
+                        .degrees(180),
+                        axis: (x: 0.0, y: 1.0, z: 0.0))
+                    .padding(5)
+                    .frame(width: 100, height: 20)
+                    .background(Color.white)
+                
+                
+                Spacer()
+            }.padding()
+            
         }.frame(width: 300, height: 200)
             .background(LinearGradient(gradient: Gradient(colors: [Color.yellow, Color.blue] ), startPoint: .leading, endPoint: .trailing))
             .cornerRadius(10)
@@ -38,6 +112,6 @@ struct CreditCardBack: View {
 
 struct CreditCard_Previews: PreviewProvider {
     static var previews: some View {
-        CreditCard<CreditCardFront>(content:  { CreditCardFront() })
+        CreditCard<CreditCardBack>(content:  { CreditCardBack() })
     }
 }
